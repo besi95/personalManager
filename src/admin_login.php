808fd67cf3c9ea215fa15a432042cadc41321381
Besim Saraci <besi@electricbot.com>
2018-01-24 17:04:43 +0100

    changes to homepage

--- src/admin_login.php
+++ src/admin_login.php
@@ -11,19 +11,20 @@ $username = mysqli_real_escape_string($conn, $_POST['admin_user']);
 $password = md5(mysqli_real_escape_string($conn, $_POST['user_password']));
 $privateToken = mysqli_real_escape_string($conn, $_POST['private_token']);
 
+
 $authenticate = authenticateAdmin($username, $password, $privateToken, $conn);
 $isAuthenticated = $authenticate['authenticated'];
 
 if ($isAuthenticated == 1) {
     $row = $authenticate['row'];
-
+die('u logua');
     session_start();
     $_SESSION['admin_logged_in'] = 1;
     $_SESSION['admin_username'] = $row['username'];
     header('location:http://localhost/paw/personalManager/views/admin_dashboard.phtml');
 
 } else {
-    die('bsi');
+    die('nuk u logua');
     $error = "Invalid Credentials!";
     header('location:http://localhost/paw/personalManager/views/admin_login.phtml');
 }
@@ -32,9 +33,9 @@ function authenticateAdmin($username, $password, $privateToken, $conn)
 {
 
     $params = array();
-    $sql = "SELECT * FROM admin WHERE username = '$username' 
-                                and password = '$password' 
-                                and secret_token = '$privateToken'";
+    $sql = "SELECT * FROM admin WHERE username = '{$username}' 
+                                and pasuord = '{$password}' 
+                                and secret_token = '{$privateToken}'";
 
     $result = mysqli_query($conn, $sql);
     $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
